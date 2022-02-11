6fb6245c57926865e285320a1437d5dd06837ba2
Sam Purkis <sam.purkis@hotmail.co.uk>
2022-02-11 20:42:42 +0000

    Perfect html to dash components

--- components/price_graph.py
+++ components/price_graph.py
@@ -14,14 +14,31 @@ def filter_html_tags(text: str) -> str:
     """
     return regex.sub(r'<[^>]*>', '', text)
 
+
 def convert_html_to_dash_components(html_string: str) -> html.Div:
     """
     Converts a html string to dash components
     :param html_string:
     :return:
     """
-    splits = re.split(r"(<[^>]*>)", html_string)  # split on html tags correctly
-    components = html.Div()
+    splits = re.split(r"(<[^>]*>(?:\w+)(?:\W+)?(?:\w+)?(?:\W+)?(?:\w+)?(?:\W+)?(?:\w+)?(?:\W+)?(?:\w+)?(?:\W+)?(?:\w+)?(?:\W+)?(?:\w+)?<\/\w+>)", html_string)  # split on html tags correctly
+    dash_converted_html = []
+    for split in splits:
+        if "<" in split:
+            href_link = re.search(r"href=\"(.*?)\"", split)
+            content = re.search(r"<[^>]*>(.*?)<\/\w+>", split)
+            if content and href_link:
+                content = content.group(1)
+                href_link = href_link.group(1)
+                dash_converted_html.append(html.A(children=content, href=href_link, target="_blank"))
+            elif content and not href_link:
+                content = content.group(1)
+                dash_converted_html.append(html.Span(children=content))
+            elif not content and not href_link:
+                dash_converted_html.append(html.Span(children=split))
+        else:
+            dash_converted_html.append(html.Span(children=split))
+    return html.Div(children=dash_converted_html)
 
 
 def make_graph(_id: str, crypto: str, crypto_info: Dict[str, Any], graph_fig) -> html.Div:
@@ -40,7 +57,7 @@ def make_graph(_id: str, crypto: str, crypto_info: Dict[str, Any], graph_fig) ->
                     html.H1(children=crypto_info["name"], style={"margin-right": "5px"}),
                     html.Img(src=crypto_info["image"]["small"]),
                 ]),
-            html.P(children=description, style={"font-size": "1.2em"}),
+            html.P(children=description_components, style={"font-size": "1.2em"}),
             dcc.Loading(
                 id=f"{crypto}-loading-1",
                 children=[
